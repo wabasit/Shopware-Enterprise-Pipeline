e3190f0bad1bce76dc4392e28c8d7ce7fc21140f
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-07-18 10:29:35 +0000

    feat: import Spark, Glue, and AWS SDK libraries for Glue job processing

---
+++ glue_jobs/transformation_pos.py
@@ -0,0 +1,14 @@
+# Import libraries: AWS, Spark, Glue, and utility modules
+import sys
+import json
+import boto3
+from datetime import datetime, timedelta, date
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
+import re
+from pyspark.sql.window import Window #
