1be1b9e10e9b871e8507d4e632a1336c9a30a0e3
richardamasah <richard.amasah@amalitechtraining.org>
2025-07-21 04:54:51 +0000

    feat: Implement initial API polling logic

--- infra/lambda_functions/api.py
+++ infra/lambda_functions/api.py
@@ -1,1 +1,14 @@
-#test
+import requests
+import json
+import boto3
+import datetime
+import time
+
+# AWS clients
+firehose = boto3.client('firehose')
+
+# Firehose stream names
+FIREHOSE_MAP = {
+    'customer-interaction': 'shopware-firehose',
+    'web-logs': 'weblogs-firehose'
+}
